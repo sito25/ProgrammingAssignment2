53af22464a9cec9e2fc5a16668e82bcdc3543c91
sito25 <m.alfonso.lopez@gmail.com>
2015-02-19 17:34:51 +0100

    R programming exercise 2 - another commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## x<-makeCacheMatrix(rbind(c(1, -1/4), c(-1/4, 1)))
 ## x$get() %*% cacheSolve(x)
 
-## A Function that creates a special  "matrix object" that stores a matrix itself,
+## A function that creates a special  "matrix object" that stores a matrix itself,
 ## its inverse ,and four methods/functions for getting access to the matrix and 
 ## its inverse. The four methods are set, get, getinverse and setinverse. 
 
@@ -24,8 +24,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## It's the function which decides what function of the object created in 
-## "makeCacheMatrix" to use. If the inverse of a particular matrix has been
-## calculated once, "cacheSolve" executes "solve" and stores the data 
+## "makeCacheMatrix" to use. If the inverse of a particular matrix hasn't been
+## calculated, "cacheSolve" executes "solve" and stores the data 
 ## otherwise get the matrix with x$getinverse. 
 
 cacheSolve <- function(x, ...) {
